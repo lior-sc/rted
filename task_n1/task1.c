bb834d1b98e355233df68e382a8ab412598f654d
lior-sc <liorsmail89@gmail.com>
2024-04-09 00:30:16 +0300

    task 1 is complete and tested

--- task_n1/task1.c
+++ task_n1/task1.c
@@ -6,15 +6,20 @@
 #include <time.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 10
 #define WAIT_TIME_MS 1000 // 1 second
 
+#define READERS_NUM 6
+#define WRITERS_NUM 2
+
 pthread_mutex_t mutex;
 atomic_int counter;
+int thread_id[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-bool wthread_running[4] = {false};
-bool rthread_running[4] = {false};
+bool wthread_running[WRITERS_NUM] = {false};
+bool rthread_running[READERS_NUM] = {false};
 
 static __uint8_t task1_buffer[100];
 
@@ -23,27 +28,21 @@ static __uint8_t task1_buffer[100];
  * @param data: pointer to a data buffer to store the current data from the shared resource
 */
 int StartRead(__uint8_t *data){
-    int ret_val = EBUSY; // default value is busy to avoid false positive response
-    clock_t start = 0;
-    const double timeout_ticks = WAIT_TIME_MS * CLOCKS_PER_SEC / 1000; // 1 second
-
-    start = clock();
-    while((double)(clock() - start) < timeout_ticks){
-        ret_val = pthread_mutex_trylock(&mutex); // try to lock the mutex
-        if(ret_val == 0){
-            memcpy(data, task1_buffer, sizeof(task1_buffer));
-            pthread_mutex_unlock(&mutex);
-            break;
-        }
-    }
-    if(ret_val == 0){
+    // lock the mutex
+    int ret = pthread_mutex_lock(&mutex); 
+    // if lock acheived, copy the data from the shared resource to the data buffer
+    if(ret == 0){
+        memcpy(data, task1_buffer, BUFFER_SIZE);
         atomic_fetch_add(&counter, 1);
+        pthread_mutex_unlock(&mutex); // unlock the mutex
+        return ret;
     }
-    else{
-        printf("Error: %d - %s\n", ret_val, strerror(ret_val));
-    }
+    // unlock the mutex
+    pthread_mutex_unlock(&mutex); // unlock the mutex
+    return ret;
 
-    return ret_val;
+
+    return ret;
 }
 
 void EndRead(){
@@ -51,22 +50,7 @@ void EndRead(){
 }
 
 int StartWrite(){
-    int ret_val = EBUSY; // default value is busy to avoid false positive response
-    clock_t start = 0;
-    const double timeout_ticks = WAIT_TIME_MS * CLOCKS_PER_SEC / 1000; // 1 second
-
-    start = clock();
-    while((double)(clock() - start) < timeout_ticks && ret_val != 0){
-         // this loop is used to wait for the mutex to be unlocked or the timeout to be reached
-        ret_val = pthread_mutex_trylock(&mutex); // try to lock the mutex
-    }
-
-    // inform the user via termminal print about the status of the start write operation
-    if(ret_val != 0){
-        printf("Failed to unlock mutex. Error: %d - %s\n", ret_val, strerror(ret_val));
-    }
-
-    return ret_val;
+    return pthread_mutex_lock(&mutex); // lock the mutex
 }
 
 void EndWrite(){
@@ -75,82 +59,121 @@ void EndWrite(){
 
 void *writeThread(void *arg){
     __uint8_t data[BUFFER_SIZE];
-    int flag_index = (int)arg;
+    int *flag_index = (int*)arg;
     
+    while(rthread_running[*flag_index] == false){
+        sleep(1);
+        printf("Waiting for the read thread %d to start\n", *flag_index);
+    }
 
-    while(wthread_running[flag_index] == true){
+    while(wthread_running[*flag_index] == true){
         time_t t0 = {0};
         t0 = clock();
-        if(StartWrite() != 0){
-            printf("thread[%d] Failed to start write operation\n",flag_index);
-            continue;
+        for (int i = 0; i < BUFFER_SIZE; i++) {
+            data[i] = rand() % 256;
         }
 
-        // Write random data to the shared resource
-        for (int i = 0; i < BUFFER_SIZE; i++) {
-            task1_buffer[i] = rand() % 256;
-        }
-        
-        // Print the data written to the shared resource
-        printf("thread[%d] wrote: ", flag_index);
-        for (int i = 0; i < BUFFER_SIZE; i++) {
-            printf("%d ", task1_buffer[i]);
-        }
-        printf("\033[65Greaders attached: %d\n", counter);
-
-        EndWrite();
-        
-        while((double)(clock() - t0) < 10e-3 * CLOCKS_PER_SEC){
-            // wait for 1ms
+        // Write thread logic here
+        int ret = StartWrite();
+        if(ret == 0 || ret == EDEADLK){
+            memcpy(task1_buffer, data, BUFFER_SIZE);
+            EndWrite();
+            printf("thread[%d] wrote: ", *flag_index);
+            for (int i = 0; i < BUFFER_SIZE; i++) {
+                printf("%d ", task1_buffer[i]);
+            }
+            printf("\033[65Greaders attached: %d\n", atomic_load(&counter));
+            usleep(100e3);
         }
 
     }
 
-    printf("Thread[%d] is exiting\n", flag_index);
+    printf("Thread[%d] is exiting\n", *flag_index);
 
     return NULL;
 }
 
-
 void *readThread(void *arg){
-    while(1);
-    // Read thread logic here
+    __uint8_t data[BUFFER_SIZE];
+    int *flag_index = (int *)arg;
+
+    while(wthread_running[*flag_index] == false){
+        // do nothing. wait for the write thread to start
+        sleep(1);
+    }
+    
+
+    while(wthread_running[*flag_index] == true){
+        __uint8_t data[BUFFER_SIZE];
+        time_t t0 = {0};
+
+        // get the current time
+        t0 = clock();
+
+        // Write thread logic heres
+        int ret = StartRead(data);
+        
+        // Print the data written to the shared resource
+        printf("thread[%d] read: ", *flag_index);
+        for (int i = 0; i < BUFFER_SIZE; i++) {
+            printf("%d ", data[i]);
+        }
+        printf("\033[65Greaders attached: %d\n", atomic_load(&counter));
+
+        usleep(300e3);
+        if(ret == 0){
+            EndRead();}
+        usleep(700e3);
+    }
+    
+    printf("Read Thread[%d] is exiting.\n", *flag_index);
+
     return NULL;
 }
 
 int main(){
-    pthread_t wthread[3];
-    pthread_t rthread[3];
+
+    pthread_t wthread[READERS_NUM];
+    pthread_t rthread[WRITERS_NUM];
     srand(time(NULL));
 
-    // Initialize and create write threads
-    for (int i = 0; i < 2; i++) {
-        wthread_running[i] = true;
-        if (pthread_create(&wthread[i], NULL, writeThread, (void*)i) != 0) {
+    // create write threads
+    for (int i = 0; i < WRITERS_NUM; i++) {
+        // create the write thread
+        if (pthread_create(&wthread[i], NULL, writeThread, (void*)&thread_id[i])) {
             printf("Failed to create write thread %d\n", i);
             return -1;
         }
         printf("WThread[%d] created\n", i);
     }
 
-    // Initialize and create read threads
-    for (int i = 0; i < 3; i++) {
+    // create read threads
+    for (int i = 0; i < READERS_NUM; i++) {
         rthread_running[i] = true;
-        if (pthread_create(&rthread[i], NULL, readThread, (void*)i) != 0) {
+        if (pthread_create(&rthread[i], NULL, readThread, (void*)&thread_id[i])) {
             printf("Failed to create read thread %d\n", i);
             return -1;
         }
         printf("RThread[%d] created\n", i);
     }
 
-    // Wait for the threads to finish
-    
+    // initialize the threads
+    for(int i=0; i<WRITERS_NUM; i++){
+        wthread_running[i] = true;
+    }
 
-    for(int i=0; i<2; i++){
+    for(int i=0; i<READERS_NUM; i++){
+        rthread_running[i] = true;
+    }
+
+    // Wait for the threads to finish
+    // start the threads
+
+    for(int i=0; i<WRITERS_NUM; i++){
         pthread_join(wthread[i], NULL);
     }
 
-    for(int i=0; i<3; i++){
+    for(int i=0; i<READERS_NUM; i++){
         pthread_join(rthread[i], NULL);
     }
 
